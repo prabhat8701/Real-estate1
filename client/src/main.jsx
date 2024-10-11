f92d96447b8872f321e30091929fb0617c8fdf93
Prabhatepic8701 <prabhat.singh@epicglobal.co.in>
2024-10-11 17:56:05 +0530

    chanfegssh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,8 +14,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      }}
      audience="https://full-stack-real-estate-youtube.vercel.app/api"
      scope="openid profile email"
-    >
+    />
       <App />
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
